5f27ed410abae7d0d5dfeed66f9752f2e7d4aebd
= <=>
2022-07-20 09:24:31 +0800

    place holder ui

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>ANIMISE</title>
+	<meta name="description" content="Anime Merchanise Online Store" />
+</svelte:head>
+
+<main class="container mx-auto pt-8  text-center">
+	<h1 class=" font-bold text-3xl md:text-5xl">Welcome to Animise</h1>
+	<p>To be done, we are just in planning stages of the UI, please wait 😞</p>
+	<img class="mx-auto w-full" src="anime.gif" alt="" />
+</main>
