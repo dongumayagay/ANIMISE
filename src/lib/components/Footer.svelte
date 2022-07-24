325210f4ba709f4d9b9a9c74421ceef7208d8005
Don Gumayagay <mcdongumayagay@gmail.com>
2022-07-24 18:50:55 +0800

    footer added

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,1 +1,63 @@
-<footer class="h-96 bg-black">tae</footer>
+<footer
+	class=" bg-black text-white flex flex-col md:flex-row gap-y-8 md:gap-x-16 border-t-2 py-16 px-8 md:px-16"
+>
+	<article class="flex-1">
+		<h1 class="text-xl font-medium pb-2">About Animise</h1>
+		<p>
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore aperiam quidem molestiae,
+			est fugit modi iste culpa a sunt architecto nesciunt harum! Ipsa, natus inventore.
+		</p>
+		<p>
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi aperiam animi facere
+			perspiciatis, fugiat culpa itaque voluptas, voluptate nihil amet illum dolorum eius debitis
+			sit.
+		</p>
+	</article>
+	<article class="flex-1">
+		<h1 class="text-xl font-medium pb-2">Follow us</h1>
+		<ul class=" flex justify-evenly items-center fill-white">
+			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+				><path
+					d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"
+				/></svg
+			>
+			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+				><path
+					d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
+				/></svg
+			>
+			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+				><path
+					d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"
+				/></svg
+			><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30">
+				<path
+					d="M24,4H6C4.895,4,4,4.895,4,6v18c0,1.105,0.895,2,2,2h18c1.105,0,2-0.895,2-2V6C26,4.895,25.104,4,24,4z M22.689,13.474 c-0.13,0.012-0.261,0.02-0.393,0.02c-1.495,0-2.809-0.768-3.574-1.931c0,3.049,0,6.519,0,6.577c0,2.685-2.177,4.861-4.861,4.861 C11.177,23,9,20.823,9,18.139c0-2.685,2.177-4.861,4.861-4.861c0.102,0,0.201,0.009,0.3,0.015v2.396c-0.1-0.012-0.197-0.03-0.3-0.03 c-1.37,0-2.481,1.111-2.481,2.481s1.11,2.481,2.481,2.481c1.371,0,2.581-1.08,2.581-2.45c0-0.055,0.024-11.17,0.024-11.17h2.289 c0.215,2.047,1.868,3.663,3.934,3.811V13.474z"
+				/></svg
+			>
+			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+				><path
+					d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"
+				/></svg
+			>
+		</ul>
+	</article>
+	<article class="flex-1">
+		<h1 class="text-xl font-medium pb-2">Helpful links</h1>
+		<h2>Privacy Policy</h2>
+		<h2>Terms and Condition</h2>
+		<h2>Customer Service</h2>
+		<h2>Shipping Information</h2>
+	</article>
+	<form class="flex-1 flex flex-col gap-y-2">
+		<h1 class="text-xl font-medium pb-2">Keep up-to-date to our latest products!</h1>
+		<input
+			placeholder="Email address"
+			class=" bg-transparent px-4 py-3 border-b border-white"
+			type="email"
+			name="email"
+			id="email"
+		/>
+		<button class=" bg-logo_red w-full px-3 py-2 font-medium" type="submit">Subscribe</button>
+	</form>
+</footer>
