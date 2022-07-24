4ab9658ad733df4ea07328576309bc64b686546b
Don Gumayagay <mcdongumayagay@gmail.com>
2022-07-24 17:35:06 +0800

    put page placeholder

---
+++ src/routes/under-construction.svelte
@@ -0,0 +1,10 @@
+<script>
+	import stop from '$lib/assets/stop.png';
+</script>
+
+<div class="section">
+	<div class="container mx-auto text-center p-8">
+		<img src={stop} alt="" class="mx-auto max-w-full" />
+		<h1 class="text-4xl uppercase font-bold">page under construciton</h1>
+	</div>
+</div>
