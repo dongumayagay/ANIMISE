35763ef5e50733241689b6cff2393f00252f98ec
Don Gumaygay <dngumayagay@gmail.com>
2022-07-26 21:05:16 +0800

    add easy way to clear localstorage in mobile

---
+++ src/routes/admin.svelte
@@ -0,0 +1,11 @@
+<div class=" grid place-items-center h-full">
+	<button
+		class="bg-red-600 text-white px-4 py-3"
+		on:click={() => {
+			localStorage.clear();
+		}}
+	>
+		<!--  -->
+		clear localStorage
+	</button>
+</div>
